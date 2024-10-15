70a29152039d4778ea6968125085a7958a266d5a
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-15 15:35:41 +0200

    test server

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const Role = db.role;
 const User = db.user; // Importer le modèle d'utilisateur
 
 // Synchroniser la base de données
-db.sequelize.sync({ alter: true }).then(() => {
+db.sequelize.sync().then(() => {
   console.log('Database synchronized successfully.');
 });
 
